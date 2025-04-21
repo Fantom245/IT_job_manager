c4491e4cecd04880d845125204d04476cbcbe116
unknown <gyrkalomas@gmail.com>
2025-04-21 22:35:40 +0300

    create simple main page

--- workspace/views.py
+++ workspace/views.py
@@ -1,2 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+from .models import Task, Worker
+
+
+# @login_required
+def index(request):
+    tasks = Task.objects.all()
+    workers = Worker.objects.all()
+
+    context = {
+        "tasks": tasks,
+        "workers": workers,
+    }
+
+    return render(request, "workspace/index.html", context=context)
