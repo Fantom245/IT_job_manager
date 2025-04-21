c32cec16224ce168c3086521982849fd7ab55dae
unknown <gyrkalomas@gmail.com>
2025-04-21 16:40:27 +0300

    create 4 models in models.py file

--- workspace/models.py
+++ workspace/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# Model for selection
+class Priority(models.TextChoices):
+    URGENT = "Urgent", "Highest priority — requires immediate attention"
+    HIGH = "High", "High priority — should be addressed soon after urgent tasks"
+    NORMAL = "Normal", "Normal priority — standard tasks with no immediate urgency"
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=500)
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=7, choices=Priority.choices, default=Priority.NORMAL)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField("Worker")
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.username})"
