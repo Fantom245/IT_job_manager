b2d081344ccb5bfdc05c5a89f1dd84c97550e7ff
unknown <gyrkalomas@gmail.com>
2025-05-01 14:13:12 +0300

    upgrade appearance search for task list

--- operation/forms.py
+++ operation/forms.py
@@ -2,4 +2,11 @@ from django import forms
 
 
 class TaskSearchForm(forms.Form):
-    name = forms.CharField(max_length=255, required=False)
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by task name"}
+        )
+    )
