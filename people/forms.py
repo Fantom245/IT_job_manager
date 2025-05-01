4d4ba26dd2ac5f52c85150431d25e7f9ef986889
unknown <gyrkalomas@gmail.com>
2025-05-01 14:12:58 +0300

    upgrade appearance search for worker list

--- people/forms.py
+++ people/forms.py
@@ -2,4 +2,11 @@ from django import forms
 
 
 class WorkerSearchForm(forms.Form):
-    username = forms.CharField(max_length=255, required=False)
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by workers name"}
+        )
+    )
