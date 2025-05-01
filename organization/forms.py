5ec1c6ad478e2866f6de56857cee62c93db22f11
unknown <gyrkalomas@gmail.com>
2025-05-01 14:29:37 +0300

    create search forms for project and team

---
+++ organization/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+
+
+class TeamSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by team name"}
+        )
+    )
+
+
+class ProjectSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by project name"}
+        )
+    )
