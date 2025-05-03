051e80dc66b5571ed4b72e3911623e5fe0c70f0b
unknown <gyrkalomas@gmail.com>
2025-05-03 22:21:00 +0300

    creating a form to add only users from added teams

--- operation/forms.py
+++ operation/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.db.models import Q
+
+from .models import Task
+from people.models import Worker
 
 
 class TaskSearchForm(forms.Form):
@@ -10,3 +14,19 @@ class TaskSearchForm(forms.Form):
             attrs={"placeholder": "Search by task name"}
         )
     )
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ["name", "description", "deadline", "priority", "task_type", "assignees"]
+
+    def __init__(self, *args, **kwargs):
+        project = kwargs.pop('project', None)
+        # Важно — передать оставшиеся kwargs (включая 'instance') в super().__init__
+        super().__init__(*args, **kwargs)
+
+        if project:
+            self.fields['assignees'].queryset = Worker.objects.filter(
+                team__in=project.teams.all()
+            ).distinct()
