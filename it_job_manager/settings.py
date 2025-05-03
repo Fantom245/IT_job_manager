35e2f2bfbd0854f5040ef108ca88d5cdd2f40e9d
unknown <gyrkalomas@gmail.com>
2025-05-03 15:58:51 +0300

    add static root

--- it_job_manager/settings.py
+++ it_job_manager/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'people.context_processors.cfg_assets_root',
             ],
         },
     },
@@ -122,6 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
+ASSETS_ROOT = "/static/assets" 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
